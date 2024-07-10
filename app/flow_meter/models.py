d1b345ec6e1ea513a4c58b2061fb1f6be17bfc77
Nalia Basova <umz-strahova@mail.ru>
2024-07-10 16:16:45 +0300

    add category

--- app/flow_meter/models.py
+++ app/flow_meter/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 
 
-
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название товара", null=True, blank=True)
-    description = models.TextField(verbose_name="Описание товара", null=True, blank=True)
-    quantity = models.IntegerField(verbose_name="Количество товара", null=True, blank=True)
-    price = models.DecimalField(max_digits=10, verbose_name="Цена товара", decimal_places=2, null=True, blank=True)
+    name = models.CharField(
+        max_length=100, verbose_name="Название товара", null=True, blank=True
+    )
+    description = models.TextField(
+        verbose_name="Описание товара", null=True, blank=True
+    )
+    quantity = models.IntegerField(
+        verbose_name="Количество товара", null=True, blank=True
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        verbose_name="Цена товара",
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    category = models.TextField(max_length=12, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
 #     class Meta:
 #         verbose_name = "Товар"
