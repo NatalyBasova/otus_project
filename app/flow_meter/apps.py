679533578549cbb63e68ac5ebd5fe5a100421171
Nalia Basova <umz-strahova@mail.ru>
2024-07-10 14:22:28 +0300

    applyed migration

--- app/flow_meter/apps.py
+++ app/flow_meter/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-# class Flow_meterConfig(AppConfig):
-#     default_auto_field = "django.db.models.BigAutoField"
-#     name = "flow_meter"
+class FlowControlConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "flow_meter"
