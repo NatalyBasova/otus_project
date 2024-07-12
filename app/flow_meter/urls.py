c7080aab5ae3a2bc1e661f3679a444429c5ffd80
Nalia Basova <umz-strahova@mail.ru>
2024-07-12 16:12:43 +0300

    add details for products

--- app/flow_meter/urls.py
+++ app/flow_meter/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("products/", views.all_products, name="products"),
+    path("products/details/<int:id>", views.product_details, name="product details"),
     # path("add_product", views.add_product, name="add_product"),
     path("about", views.about, name="about"),
 ]
--- app/flow_meter/views.py
+++ app/flow_meter/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect
 
 
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from .models import Product
 
 # from .forms import ProductForm
 
 
-def index(request) -> HttpResponse:
+def index(request: HttpRequest) -> HttpResponse:
     # products = Product.objects.order_by("-id")
     # context = {"title": "Главная страница сайта", "products": products}
     # return render(request, "templates/index.html", context)
@@ -17,13 +17,23 @@ def index(request) -> HttpResponse:
     return render(request, "index.html")
 
 
-def all_products(request) -> HttpResponse:
+def all_products(request: HttpRequest) -> HttpResponse:
     products = Product.objects.all().values()
 
     return render(
         request=request,
         template_name="all_products.html",
         context={"products": products},
+    )
+
+
+def product_details(request: HttpRequest, id: int) -> HttpResponse:
+    product = Product.objects.get(id=id)
+
+    return render(
+        request=request,
+        template_name="product_details.html",
+        context={"product": product},
     )
 
 
@@ -42,5 +52,5 @@ def all_products(request) -> HttpResponse:
 #         return render(request, "templates/add_product.html", context)
 
 
-def about(request):
+def about(request: HttpRequest) -> HttpResponse:
     return render(request, "about.html")
