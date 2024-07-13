0d5d49c5d516f4e8a533b7d7f0abc61fcce87c69
Nalia Basova <umz-strahova@mail.ru>
2024-07-13 20:50:15 +0300

    add product view

--- app/flow_meter/views.py
+++ app/flow_meter/views.py
@@ -3,17 +3,12 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
-from .models import Product
 
-# from .forms import ProductForm
+from .models import Product
+from .forms import ProductForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    # products = Product.objects.order_by("-id")
-    # context = {"title": "Главная страница сайта", "products": products}
-    # return render(request, "templates/index.html", context)
-    # template = loader.get_template( "templates/index.html")
-    # return HttpResponse(template.render())
     return render(request, "index.html")
 
 
@@ -37,19 +32,19 @@ def product_details(request: HttpRequest, id: int) -> HttpResponse:
     )
 
 
-# def add_product(request):
-#     error = ""
-#     if request.method == "POST":
-#         form = ProductForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("index")
-#         else:
-#             error = "Форма была не верной"
-#         form = ProductForm()
-#         context = {"form": form, "error": error}
+def add_product(request: HttpRequest) -> HttpResponse:
+    error = ""
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("products")
+        else:
+            error = "Форма была неверной"
+        form = ProductForm()
+        context = {"form": form, "error": error}
 
-#         return render(request, "templates/add_product.html", context)
+    return render(request=request, template_name="add_product.html", context=context)
 
 
 def about(request: HttpRequest) -> HttpResponse:
