5ab7d7109f51145324057e6f6689544ff478dae9
Nalia Basova <umz-strahova@mail.ru>
2024-07-21 15:11:03 +0300

    create

--- app/flow_meter/tests/models.py
+++ app/flow_meter/tests/models.py
@@ -1,15 +1,13 @@
 from django.test import TestCase
 from flow_meter.models import Product
 
+
 class TestProduct(TestCase):
     def setUp(self):
         self.product = Product.objects.create(
-            name='pen',
-            
+            name="pen",
+            quantity=12,
         )
-     
 
     def tearDown(self):
-        print('Я выполняюсь после каждого теста')
-
-
+        print("Я выполняюсь после каждого теста")
