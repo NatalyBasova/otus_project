cc7bebe67ed7194a3c2f3084855c100ba8f07ce5
Nalia Basova <umz-strahova@mail.ru>
2024-07-29 10:22:23 +0300

    clean

--- app/flow_meter/views.py
+++ app/flow_meter/views.py
@@ -22,8 +22,7 @@ def index(request: HttpRequest) -> HttpResponse:
 class ProductListView(ListView):
     model = Product
     paginate_by = 10
-    
-  
+
 
 class ProductDetailView(DetailView):
     model = Product
@@ -47,78 +46,8 @@ class ProductDeleteView(DeleteView):
     success_url = reverse_lazy("products")
 
 
-# def all_products(request: HttpRequest) -> HttpResponse:
-#     products = Product.objects.all().values()
-
-#     return render(
-#         request=request,
-#         template_name="all_products.html",
-#         context={"products": products},
-#     )
-
-
-# def product_details(request: HttpRequest, id: int) -> HttpResponse:
-#     product = Product.objects.get(id=id)
-
-#     return render(
-#         request=request,
-#         template_name="product_details.html",
-#         context={"product": product},
-#     )
-
-
-# def product_delete(request: HttpRequest, id: int) -> HttpResponse:
-#     product = Product.objects.get(id=id)
-
-#     if request.method == "POST":
-#         product.delete()
-#         return redirect("products")
-
-#     return render(
-#         request=request,
-#         template_name="product_delete.html",
-#         context={"product": product},
-#     )
-
-
-# def product_update(request: HttpRequest, id: int) -> HttpResponse:
-
-#     product = Product.objects.get(id=id)
-#     error = ""
-
-#     if request.method == "POST":
-#         form = ProductForm(request.POST, instance=product)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("products")
-#         else:
-#             error = "Форма была неверной"
-#     else:
-#         form = ProductForm(instance=product)
-
-#     context = {"form": form, "error": error}
-
-#     return render(request=request, template_name="product_update.html", context=context)
-
-
-# def product_add(request: HttpRequest) -> HttpResponse:
-#     error = ""
-#     if request.method == "POST":
-#         form = ProductForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("products")
-#         else:
-#             error = "Форма была неверной"
-
-#     form = ProductForm()
-#     context = {"form": form, "error": error}
-
-#     return render(request=request, template_name="product_add.html", context=context)
-
-
-# def about(request: HttpRequest) -> HttpResponse:
-#     return render(request, "about.html")
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "about.html")
 
 
 # def all_categories(request: HttpRequest) -> HttpResponse:
