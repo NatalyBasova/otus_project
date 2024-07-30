9847f28d0611ea405b2bd9b43b7db1e4b8004b3e
NatalyBasova <161022788+NatalyBasova@users.noreply.github.com>
2024-07-30 23:36:14 +0300

    Link products and categories (#6)
    * link with category
    * add navbar

--- app/flow_meter/forms.py
+++ app/flow_meter/forms.py
@@ -1,11 +1,11 @@
 from .models import Product, Category
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea, Select
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "description", "quantity", "price"]
+        fields = ["name", "description", "quantity", "price", "category"]
         widgets = {
             "name": TextInput(
                 attrs={"class": "form-control", "placeholder": "Введите товар"}
@@ -18,6 +18,9 @@ class ProductForm(ModelForm):
             ),
             "price": TextInput(
                 attrs={"class": "form-control", "placeholder": "Введите цену товара"}
+            ),
+            "category": Select(
+                attrs={"class": "form-control", "placeholder": "Выберите категорию"}
             ),
         }
 
