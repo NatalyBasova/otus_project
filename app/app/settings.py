bb75b4f8a7b99fbaa325c674aca5adcbcbfd982d
Nalia Basova <umz-strahova@mail.ru>
2024-08-01 21:06:58 +0300

    settings for users

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "flow_meter",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "flow_meter/")
+
+AUTH_USER_MODEL = 'users.MyUser'
+
+LOGIN_URL = '/users/login/'
+LOGOUT_REDIRECT_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("flow_meter.urls")),
+    path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
 ]
 # if settings.DEBUG:
