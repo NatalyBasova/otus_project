9a2e1ffd4b7270f9cc4e9063868edaeda297d0d2
Nalia Basova <umz-strahova@mail.ru>
2024-08-01 23:56:49 +0300

    permissions

--- app/flow_meter/views.py
+++ app/flow_meter/views.py
@@ -14,8 +14,8 @@ from django.urls import reverse_lazy, reverse
 from .models import Product, Category
 from .forms import ProductForm, CategoryForm
 
-
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+# from django.contrib.auth.decorators import login_required, permission_required
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -56,16 +56,20 @@ def about(request: HttpRequest) -> HttpResponse:
 class CategoryListView(ListView):
     model = Category
     paginate_by = 10
+    permission_required = "flow_meter.view_category"
 
 
 class CategoryDetailView(DetailView):
     model = Category
+    permission_required = "flow_meter.view_category"
 
 
-class CategoryCreateView(CreateView):
+class CategoryCreateView(PermissionRequiredMixin, CreateView):
     model = Category
     form_class = CategoryForm
     success_url = reverse_lazy("categories")
+    permission_required = "flow_meter.add_category"
+    
 
 
 class CategoryUpdateView(UpdateView):
@@ -73,8 +77,10 @@ class CategoryUpdateView(UpdateView):
     form_class = CategoryForm
     success_url = reverse_lazy("categories")
     template_name = "flow_meter/category_update.html"
+    permission_required = "flow_meter.change_category"
 
 
 class CategoryDeleteView(DeleteView):
     model = Category
     success_url = reverse_lazy("categories")
+    permission_required = "flow_meter.delete_category"
