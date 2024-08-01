f4d7b23e57fe7060134bf42fe1c30c8a98a88eda
Nalia Basova <umz-strahova@mail.ru>
2024-08-01 23:32:08 +0300

    rm users

--- app/users_bak/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
-
-from .models import User
-
-class AuthView(LoginView):
-    template_name = 'users/login.html'
