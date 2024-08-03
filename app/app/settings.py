c272b1d5243ad139dfcb868624dcbcf8d0cbd4a9
NatalyBasova <161022788+NatalyBasova@users.noreply.github.com>
2024-08-03 23:37:22 +0300

    Github actions (#9)
    * add GH actions file
    * test models

--- app/app/settings.py
+++ app/app/settings.py
@@ -92,19 +92,18 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "./db.sqlite3")
 DATABASE_USER = os.environ.get("DATABASE_USER", "")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD", "")
 DATABASE_HOST = os.environ.get("DATABASE_HOST", "127.0.0.1")
 DATABASE_PORT = os.environ.get("DATABASE_PORT", "5432")
+# on prod use django.db.backends.postgresql
+DB_ENGINE = os.environ.get("DJANGO_DB_ENGINE", "django.db.backends.sqlite3")
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": DB_ENGINE,
+        # for PG pass DATABASE_NAME="some_pg_database"
         "NAME": DATABASE_NAME,
         "USER": DATABASE_USER,
         "PASSWORD": DATABASE_PASSWORD,
